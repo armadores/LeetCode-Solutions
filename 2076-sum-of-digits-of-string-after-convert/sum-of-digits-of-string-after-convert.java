3dab92be754d0b91609115bd7635ba3649cfd3ce
Emre Cagri GOKMEN <66444073+armadores@users.noreply.github.com>
2023-09-12 09:45:02 +0300

    Time: 1 ms (91.50%) | Memory: 40.3 MB (94.47%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.java
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.java
@@ -1,18 +1,10 @@
 class Solution {
     public int getLucky(String s, int k) {
-        // Create a map to store sorting values for each character in the alphabet
-        Map<Character, Integer> sortingValues = new HashMap<>();
-        
-        // Assign sorting values from 1 to 26 based on character position
-        for (char ch = 'a'; ch <= 'z'; ch++) {
-            sortingValues.put(ch, ch - 'a' + 1);
-        }
-
         int num = 0;
 
         // Convert characters to integers and sum them
         for (char c : s.toCharArray()) {
-            int value = sortingValues.get(c);
+            int value = c - 'a' + 1;
             while (value > 0) {
                 num += value % 10;
                 value /= 10;
@@ -32,3 +24,4 @@ class Solution {
         return num;
     }
 }
+
